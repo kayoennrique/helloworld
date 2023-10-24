7a6d0e5e10f2112b5ce189492fc879d77829962f
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-10-24 14:01:50 -0300

    installing react-router-dom and creating router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/aboutme" element={<AboutMe />}/>
+      <Route path="*" element={<div>Page Not Found</div>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
